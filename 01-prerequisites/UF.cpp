803125dbe5b8fbe4a89e74d3003f97421934f372
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2022-12-05 17:57:14 +0100

    Add UF.cpp

---
+++ 01-prerequisites/UF.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <set>
+typedef long long LL;
+
+// Weighted quick-union with path compresson
+class UF
+{
+  private:
+    std::vector<LL> id;
+    std::vector<LL> size;
+  public:
+    LL count;
+
+    UF(LL N) {
+      count = N;
+      for (LL i = 0; i < N; i++) {
+        id.push_back(i);
+        size.push_back(1);
+      }
+    };
+
+    LL find(LL p) {
+      std::set<LL> seen;
+      while (p != id[p]) { p = id[p]; seen.insert(p); }
+      for (LL s : seen) id[s] = p; // path compression
+      return p;
+    };
+
+    void _union(LL p, LL q) {
+      LL i = find(p);
+      LL j = find(q);
+      if (i == j) return;
+
+      if (size[i] < size[j]) { id[i] = j; size[j] += size[i]; }
+      else                   { id[j] = i; size[i] += size[j]; }
+      count--;
+    }
+};
