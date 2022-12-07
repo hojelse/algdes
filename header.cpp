00dd934fdb47909675efd3763e7bc2c9183c0d68
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2022-12-07 23:01:51 +0100

    Update header.cpp

--- header.cpp
+++ header.cpp
@@ -1,11 +1,17 @@
+// #include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
-#include <string>
-#include <set>
-#include <map>
-#include <algorithm>
-typedef long long LL;
-typedef long double F;
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lf;
+typedef vector<ll> vi;
+typedef vector<vi> mi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+typedef vector<vii> mii;
+const int INF = ~(1<<31);
+template <class T> T smod(T a, T b) { return (a % b + b) % b; }
 int main(void) {
   return 0;
 }
